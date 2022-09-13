59b863e150cb4a6c6e60614c9dc926f3bd709dae
Luiz Guilherme <guilhermerx18@gmail.com>
2022-09-13 18:43:03 -0300

    feat: middleware error

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import "./Models/connection"
-import userRouter from "./Routes/user.route"
+import { NextFunction, Request, Response } from "express";
+import 'express-async-errors';
+import "./Models/connection";
+import userRouter from "./Routes/user.route";
 
 const express = require('express')
 const app = express()
@@ -7,5 +9,9 @@ const port = 3000
 
 app.use(express.json())
 app.use('/user', userRouter);
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const { message } = err;
+  return res.status(404).json({ message })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
